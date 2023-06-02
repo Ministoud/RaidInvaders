77a9dab83b3d968dbe96562e079afc7ece73767c
Ministoud <ministoud@gmail.com>
2023-06-02 02:07:43 +0200

    [feature/Invaders] Custom events + adding random shoots to Invaders
    + Creating a very simple Singleton helper that allows custom events throw the entire app

--- js/game/Invader.js
+++ js/game/Invader.js
@@ -1,4 +1,6 @@
 import { Entity } from "./Entity.js";
+import { Projectile } from "./Projectile.js";
+import { EventHelper } from "../utils/EventHelper.js";
 
 export class Invader extends Entity {
     constructor(posX) {
@@ -21,9 +23,24 @@ export class Invader extends Entity {
             y: 1,
         };
         super(initialPosition, initialVelocity);
+        this.timePreviousShot = Date.now();
+        this.timeBeforeNextShot = Math.random() * 1000;
+    }
+
+    shoot() {
+        const projectileVelocity = 1;
+        this.timePreviousShot = Date.now();
+        this.timeBeforeNextShot = Math.random() * 5000;
+
+        const eventHelper = new EventHelper();
+        eventHelper.sendEvent("addEntity", new Projectile(this.position.x + this.width / 2, this.position.y + this.height, projectileVelocity + this.velocity.y, this));
     }
 
     update(canvasContext) {
+        if (Date.now() - this.timePreviousShot > this.timeBeforeNextShot) {
+            this.shoot();
+        }
+
         this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
 
--- js/game/game.js
+++ js/game/game.js
@@ -1,22 +1,17 @@
 import { Player } from './Player.js';
 import { Projectile } from './Projectile.js';
 import { Invader } from './Invader.js';
+import { EventHelper } from '../utils/EventHelper.js';
 
 const canvas = document.querySelector('canvas');
 const canvasContext = canvas.getContext('2d');
+const eventHelper = new EventHelper();
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 // Contains all entities in the game
 let entities = [];
-
-// Start the game
-requestAnimationFrame(update);
-
-// Spawn a player
-const player = new Player();
-entities.push(player);
 
 function update() {
     requestAnimationFrame(update);
@@ -25,10 +20,7 @@ function update() {
         entity.update(canvasContext);
         // Check if the entity is out of the screen and remove it if so
         if (entity.position.y > canvas.height || entity.position.y < 0 || entity.position.x < 0 || entity.position.x > canvas.width) {
-            // Avoid flickering when removing entities
-            setTimeout(() => {
-                entities.splice(entities.indexOf(entity), 1);
-            }, 0);
+            eventHelper.sendEvent("removeEntity", entity);
         }
     });
 
@@ -36,22 +28,42 @@ function update() {
     canvasContext.fillText(`Entities: ${entities.length}`, 10, 20);
 }
 
+
 // Detect mouse mouvement and change player's position accordingly without letting the player go out of the canvas
-addEventListener('mousemove', ({ clientX }) => {
+eventHelper.listenEvent('mousemove', ({ clientX }) => {
     player.moveTo(clientX);
 });
 
 // Detect mouse click and shoot a projectile
-addEventListener('mouseup', () => {
+eventHelper.listenEvent('mouseup', () => {
     const projectileVelocity = -10;
     const projectile = new Projectile(player.position.x + player.width / 2, player.position.y, projectileVelocity, player);
-    entities.push(projectile);
+    eventHelper.sendEvent("addEntity", projectile);
 })
 
-addEventListener('keydown', ({ key }) => {
+// TEMP EVENT FOR TESTING
+eventHelper.listenEvent('keydown', ({ key }) => {
     if (key === ' ') {
         const invaderPosX = Math.random() * canvas.width;
         const invader = new Invader(invaderPosX);
-        entities.push(invader);
+        eventHelper.sendEvent("addEntity", invader);
     }
+});
+
+eventHelper.listenEvent('addEntity', ({ detail }) => {
+    entities.push(detail);
 })
+
+eventHelper.listenEvent('removeEntity', ({ detail }) => {
+    // Avoid flickering when removing entities
+    setTimeout(() => {
+        entities.splice(entities.indexOf(detail), 1);
+    }, 0);
+})
+
+// Spawn a player
+const player = new Player();
+eventHelper.sendEvent("addEntity", player);
+
+// Start the game
+requestAnimationFrame(update);
